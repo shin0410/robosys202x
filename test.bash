4a94359f6a54e3bc4bb2370a63b4df95d84f1de7
Yamamoto Shintarou <shin_yamamoto@i.softbank.jp>
2022-10-27 12:39:32 +0900

    change test

--- test.bash
+++ test.bash
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-seq 5 | python3 plus
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | python3 plus)
+
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "${res}" = 0 ] && echo OK
+exit $res
+
 
