18dc7bef091bdd4b6d035e67a01eff0c065c5059
Yamamoto Shintarou <shin_yamamoto@i.softbank.jp>
2022-10-29 10:53:42 +0900

    Change a test

--- test.bash
+++ test.bash
@@ -1,5 +1,6 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Yamamoto Shintarou
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo NG at Line $1
@@ -12,6 +13,15 @@ res=0
 out=$(seq 5 | python3 plus)
 
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | python3 plus)
+[ "$?" = 1 ]     || ng ${LINENO}
+[ "${out}" = ""] || ng ${LINENO}
+
+out=$(echo | python3 plus)
+[ "$?" = 1 ]     || ng ${LINENO}
+[ "${out}" = ""] || ng ${LINENO}
 
 [ "${res}" = 0 ] && echo OK
 exit $res
